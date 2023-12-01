0db58c68436454a10c07155da9585241a5c1b62a
Muhammad Asif <125912946+MuhammadAsif46@users.noreply.github.com>
2023-12-01 12:15:54 +0500

    hello world express

---
+++ 2.expressJs hello world/server.mjs
@@ -0,0 +1,127 @@
+import express from 'express';
+// import cors from 'cors';
+import path from 'path';
+// const __dirname = path.resolve();
+
+const app = express();
+app.use(express.json()); // body parser
+// app.use(cors())
+
+
+app.get('/profile', (req, res) => {
+    console.log('this is profile!', new Date());
+    res.send('this is profile: ' + new Date());
+})
+
+
+//  http://abcweather.com/weather/karachi?unit=metric&side=west&age=23
+
+app.get('/weather/:cityName', (req, res) => {
+    console.log('this is profile!', new Date());
+
+    console.log("req.params.cityName: ", req.params.cityName)
+
+    console.log("req.query.unit: ", req.query.unit)
+    console.log("req.query.side: ", req.query.side)
+    console.log("req.query.age: ", req.query.age)
+
+    console.log("req.body.name: ", req.body.name)
+    console.log("req.body.class: ", req.body.class)
+    console.log("req.body.rollNumber: ", req.body.rollNumber)
+
+
+
+
+    let weatherData = {
+        karachi: {
+            city: 'karachi',
+            tempInC: 30,
+            humidity: 56,
+            high: 32,
+            low: 18
+        },
+        london: {
+            city: 'london',
+            tempInC: 5,
+            humidity: 56,
+            high: 32,
+            low: 18
+        },
+    }
+
+    let userInputCityName = req.params.cityName.toLowerCase();
+    console.log("userInputCityName: ", userInputCityName);
+
+    let weatherDataToSend = weatherData[userInputCityName];
+
+    if (weatherDataToSend) {
+        res.send(weatherDataToSend);
+    } else {
+        res.status(404)
+            .send(`weather data is not available for ${req.params.cityName}`);
+    }
+})
+
+let comments = [];
+
+app.post('/comment/:name', (req, res, next) => {
+    const name = req.params.name;
+    const comment = req.body.comment;
+
+    comments.unshift({
+        name: name,
+        comment: comment
+    })
+
+    res.send({
+        message: "comment posted successfully"
+    });
+
+})
+app.get('/comments', (req, res, next) => {
+    res.send(comments);
+})
+
+
+app.use(express.static(path.join(process.cwd, 'public')))
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Example server listening on port ${PORT}`)
+})
+
+
+// google cloud - Google Compute engine;
+// AWS - ES2 instance
+
+// micro service architecture
+// google app engine - cyclic - railway app - heroku - aws elastic beanstalk
+
+// very cheap - per call charge - short lived
+// nano service architecture
+// google cloud functions - aws lambda functions - netlify functions
+
+
+
+
+
+// GET     /api/v1/post/:userId/:postId
+// GET     /api/v1/posts/:userId
+// POST    /api/v1/post
+// PUT     /api/v1/post/:userId/:postId
+// DELETE  /api/v1/post/:userId/:postId
+
+// GET     /api/v2/post/:userId/:postId
+// GET     /api/v2/posts/:userId
+// POST    /api/v2/post
+// PUT     /api/v2/post/:userId/:postId
+// DELETE  /api/v2/post/:userId/:postId
+
+// GET     /v1/comment/:userId/:commentId
+// GET     /v1/comments/:userId
+// POST    /v1/comment
+// PUT     /v1/comment/:userId/:commentId
+// DELETE  /v1/comment/:userId/:postId
+
+
+// GET     /feed/:userId
